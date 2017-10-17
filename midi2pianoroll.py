a008be881bec949144a54f64ef2ab6fd993036f8
salu133445 <salu133445@gmail.com>
2017-10-17 19:12:31 +0800

    midi2pianoroll is done!!

--- midi2pianoroll.py
+++ midi2pianoroll.py
@@ -1,23 +1,3 @@
-# Herman Dong 2017.5.18 v1
-# Last modified: Herman Dong 2017.09.27 v2
-"""
-[How to use]
-    (1) set the correct DATASET_ROOT, SEARCH_ROOT, RESULTS_PATH constant
-    (2) set the correct SCORE_FILE path constant
-    (3) set the desired FILETYPE constant
-    (4) set the correct CONFIDENCE_TH, BEAT_RESOLUTION constant
-    (5) run the following
-            python2 midi2pianoroll.py
-[Notes]
-    Only the midi file with highest confidence score is chosen.
-    occurrence ratio: the ratio of the number of bars that the
-        instrument shows up to the number of bars in total
-    average notes simultaneously: the average number of notes occurs simultaneously
-    max notes simultaneously: the maximum number of notes occurs simultaneously
-    rhythm complexity: average number of notes(onset only) in a bar
-    pitch complexity: average number of notes with different pitches
-"""
-
 from __future__ import print_function
 import os
 import shutil
@@ -137,8 +117,7 @@ def get_tempo_info_and_array_dict(pm, beat_times=None):
 def get_midi_info_and_array_dict(pm):
     """Given a pretty_midi.PrettyMIDI class instance, return its midi_info_dict and midi_array_dict."""
     # get time sigature changes info
-    time_signature_info_dict, time_signature_array_dict = get_time_signature_info_and_array_dict(pm, \
-                                                            beat_times=beats_array_dict['beat_times'])
+    time_signature_info_dict, time_signature_array_dict = get_time_signature_info_and_array_dict(pm)
     # get beats info dictionary and beats array dictionary
     beats_info_dict, beats_array_dict = get_beats_info_and_array_dict(pm, sort_tsc=False)
     # get tempo info dictionary and tempo array dictionary
@@ -156,7 +135,7 @@ def get_piano_roll(instrument, beat_times=None, tempo_array=None, pm=None):
         beat_times = pm.get_beats(beat_start_time)
         num_beats = len(beat_times)
     if tempo_array is None:
-        ~, tempo_array = get_tempo_info_and_array_dict(pm, beat_times)
+        _, tempo_array = get_tempo_info_and_array_dict(pm, beat_times)
     # create the piano roll and the onset array
     if settings['velocity'] == 'bool':
         piano_roll = np.zeros(shape=(settings['beat_resolution']*num_beats, 128), dtype=bool)
@@ -166,56 +145,38 @@ def get_piano_roll(instrument, beat_times=None, tempo_array=None, pm=None):
     # calculate pixel per beat
     ppbeat = settings['beat_resolution']
     hppbeat = settings['beat_resolution']/2
-    # iterate through all the notes in the instrument track
-    if not instrument.is_drum:
-        for note in instrument.notes:
-            if note.end < beats[0]:
-                continue
+    # iterate through notes
+    for note in instrument.notes:
+        if note.end < beat_times[0]:
+            continue
+        else:
+            # find the corresponding index of the note on/off event
+            if note.start >= beat_times[0]:
+                start_beat = np.searchsorted(beat_times, note.start, side='right') - 1
+                start_loc = (note.start - beat_times[start_beat])
+                start_loc = start_loc * tempo_array[int(start_beat*hppbeat)] / 60.0
             else:
-                # find the corresponding index of the note on/off event
-                if note.start >= beats[0]:
-                    start_beat = np.searchsorted(beats, note.start, side='right') - 1
-                    start_loc = (note.start - beats[start_beat])
-                    start_loc = start_loc * tempo_array[int(start_beat*hppbeat)] / 60.0
-                else:
-                    start_beat = 0
-                    start_loc = 0.0
-                end_beat = np.searchsorted(beats, note.end, side='right') - 1
-                end_loc = (note.end - beats[end_beat])
+                start_beat = 0
+                start_loc = 0.0
+            start_idx = int(start_beat*ppbeat + start_loc*hppbeat)
+
+            if instrument.is_drum:
+                end_idx = start_idx + 2
+            else:
+                end_beat = np.searchsorted(beat_times, note.end, side='right') - 1
+                end_loc = (note.end - beat_times[end_beat])
                 end_loc = end_loc * tempo_array[int(end_beat*hppbeat)] / 60.0
-                start_idx = int(start_beat*ppbeat + start_loc*hppbeat)
                 end_idx = int(end_beat*ppbeat + end_loc*hppbeat)
                 # make sure the note length is larger than minimum note length
                 if end_idx - start_idx < 2:
                     end_idx = start_idx + 2
-                # set values to the pianoroll matrix and the onset array
-                if settings['velocity'] == 'bool':
-                    piano_roll[start_idx:(end_idx-1), note.pitch] = True
-                elif settings['velocity'] == 'int':
-                    piano_roll[start_idx:(end_idx-1), note.pitch] = note.velocity
-                if start_idx < onset_array.shape[0]:
-                    onset_array[start_idx, 0] = True
-    else: # drums track
-        for note in instrument.notes:
-            if note.end < beats[0]:
-                continue
-            else:
-                # find the corresponding index of the note on event (for drums, only note on events are captured)
-                if note.start >= beats[0]:
-                    start_beat = np.searchsorted(beats, note.start, side='right') - 1
-                    start_loc = (note.start - beats[start_beat])
-                    start_loc = start_loc * tempo_array[int(start_beat*ppbeat)] / 60.0
-                else:
-                    start_beat = 0
-                    start_loc = 0.0
-                start_idx = int((start_beat+start_loc)*ppbeat)
-                # set values to the pianoroll matrix and the onset array
-                if settings['velocity'] == 'bool':
-                    piano_roll[start_idx:start_idx+1, note.pitch] = True
-                elif settings['velocity'] == 'int':
-                    piano_roll[start_idx:start_idx+1, note.pitch] = note.velocity
-                if start_idx < onset_array.shape[0]:
-                    onset_array[start_idx, 0] = True
+            # set values to the pianoroll matrix and the onset array
+            if settings['velocity'] == 'bool':
+                piano_roll[start_idx:(end_idx-1), note.pitch] = True
+            elif settings['velocity'] == 'int':
+                piano_roll[start_idx:(end_idx-1), note.pitch] = note.velocity
+            if start_idx < onset_array.shape[0]:
+                onset_array[start_idx, 0] = True
     return piano_roll, onset_array
 
 def get_instrument_info(instrument, identifier):
@@ -225,16 +186,16 @@ def get_instrument_info(instrument, identifier):
             'program name': pretty_midi.program_to_instrument_name(instrument.program),
             'name': instrument.name,
             'is drum': instrument.is_drum,
-            'family number': int(pretty_midi.program)//8,
+            'family number': int(instrument.program)//8,
             'family name': pretty_midi.program_to_instrument_class(instrument.program)}
 
 def get_instrument_filename(instrument, identifier):
     """Given a pretty_midi.Instrument class instance and the identifier, return the filename of the instrument."""
     family_name = pretty_midi.program_to_instrument_class(instrument.program)
     program_name = pretty_midi.program_to_instrument_name(instrument.program)
-    return '_'.join(str(idx), family_name, program_name)
+    return '_'.join([str(identifier), family_name, program_name])
 
-def get_piano_roll_list(pm):
+def get_piano_rolls(pm):
     """
     Get the piano-rolls of the given pretty_midi object.
 
@@ -246,35 +207,38 @@ def get_piano_roll_list(pm):
             piano_rolls: a list of extracted piano-rolls, one for each instrument track
             onset_arrays: a list of extracted onset_arrays, one for each instrument track, having one-to-one
                 correspondence with piano_rolls
-            midi_info_dict: a dictionary containing information of the midi file, including time_signature/beats/tempo/info
-            instrument_dict: a dictionary containing information of each track
             midi_array_dict: a dictionary containing informative arrays as listed following
-                beats_time: an array storing the time (in sec) of each beat in the original midi file
+                beat_times: an array storing the time (in sec) of each beat in the original midi file
                 downbeats_time: an array storing the time (in sec) of each downbeat in the original midi file
                 tempo_array: an array storing the tempo at each time step
                 beats_array: an array storing the location (time stpe) of beats
                 downbeats_array: an array storing the location (time stpe) of beats
+            midi_info_dict: a dictionary containing information of the midi file (time_signature/beats/tempo info)
+            instrument_dict: a dictionary containing information of each track
     """
     # create an empty instrument dictionary to store information of each instrument
     instrument_info_dict = {}
     piano_rolls = []
     onset_arrays = []
+    # get the midi information and the beats/tempo arrays
+    midi_info_dict, midi_array_dict = get_midi_info_and_array_dict(pm)
     # sort instruments by their program numbers
     pm.instruments.sort(key=lambda x: x.program)
     # iterate thorugh all instruments
     for instrument, idx in enumerate(pm.instruments):
         # get the piano_roll and the onset array of a specific instrument
-        piano_roll, onset_array = get_piano_roll(pm, instrument, beats=beats, tempo_array=tempo_array)
+        piano_roll, onset_array = get_piano_roll(instrument, beat_times=midi_array_dict['beat_times'],
+                                                 tempo_array=midi_array_dict['tempo_array'])
         # append the piano-roll to the piano_roll list and the onset_array list
-        piano_roll_list.append(piano_roll)
+        piano_rolls.append(piano_roll)
         onset_arrays.append(onset_array)
         # append information of current instrument to instrument dictionary
         instrument_info = get_instrument_info(instrument, idx)
-        instrument_filename = get_instrument_filename(instrument, idx))
+        instrument_filename = get_instrument_filename(instrument, idx)
         instrument_info_dict[instrument_filename] = instrument_info
-    return piano_rolls, onset_arrays, instrument_info_dict
+    return piano_rolls, onset_arrays, midi_array_dict, midi_info_dict, instrument_info_dict
 
-def midi_to_pianoroll(midi_path):
+def midi_to_pianorolls(midi_path):
     """
     Convert a midi file to piano-rolls of multiple tracks.
 
@@ -286,19 +250,15 @@ def midi_to_pianoroll(midi_path):
             piano_rolls: a list of extracted piano-rolls, one for each instrument track
             onset_arrays: a list of extracted onset_arrays, one for each instrument track, having one-to-one
                 correspondence with piano_rolls
-            midi_info_dict: a dictionary containing information of the midi file, including time_signature/beats/tempo/info
-            instrument_dict: a dictionary containing information of each track
             midi_array_dict: a dictionary containing informative arrays as listed following
                 beats_time: an array storing the time (in sec) of each beat in the original midi file
                 downbeats_time: an array storing the time (in sec) of each downbeat in the original midi file
                 tempo_array: an array storing the tempo at each time step
                 beats_array: an array storing the location (time stpe) of beats
                 downbeats_array: an array storing the location (time stpe) of beats
+            midi_info_dict: a dictionary containing information of the midi file (time_signature/beats/tempo info)
+            instrument_info_dict: a dictionary containing information of each track
     """
     # load the MIDI file as a pretty_midi object
     pm = pretty_midi.PrettyMIDI(midi_path)
-    # get the midi information and the beats/tempo arrays
-    midi_info_dict, midi_array_dict = get_midi_info_and_info_arrays(pm)
-    # extract all the instrument tracks into piano rolls
-    piano_rolls, onset_arrays, instrument_dict = get_piano_rolls(pm)
-    return piano_rolls, onset_arrays, midi_array_dict, midi_info_dict, instrument_info_dict
+    return get_piano_rolls(pm)
