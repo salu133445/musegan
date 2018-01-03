47097917bc6c3b7051b1eaaff0464e889d9ea74b
salu133445 <b02901080@ntu.edu.tw>
2018-01-04 02:19:44 +0800

    update with merge_tracks

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,4 +1,10 @@
+import os
 import yaml
 
 with open("settings.yaml", "r") as config_file:
     settings = yaml.load(config_file)
+
+settings['lmd_path'] = os.path.join(settings['lmd_dir'], 'lmd_'+settings['dataset'])
+settings['lpd_path'] = os.path.join(settings['lpd_dir'], 'lpd_'+settings['dataset'])
+settings['lpd_5_path'] = os.path.join(settings['lpd_dir'], 'lpd_5_'+settings['dataset'])
+settings['lpd_5_cleansed'] = os.path.join(settings['lpd_dir'], 'lpd_5_cleansed')
