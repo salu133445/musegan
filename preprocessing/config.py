6454016f0d3072da2a6ad68ae2d00465ebea34d9
salu133445 <b02901080@ntu.edu.tw>
2018-01-08 19:07:49 +0800

    add cleansing config

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -3,8 +3,3 @@ import yaml
 
 with open("settings.yaml", "r") as config_file:
     settings = yaml.load(config_file)
-
-settings['lmd_path'] = os.path.join(settings['lmd_dir'], 'lmd_'+settings['dataset'])
-settings['lpd_path'] = os.path.join(settings['lpd_dir'], 'lpd_'+settings['dataset'])
-settings['lpd_5_path'] = os.path.join(settings['lpd_dir'], 'lpd_5_'+settings['dataset'])
-settings['lpd_5_cleansed'] = os.path.join(settings['lpd_dir'], 'lpd_5_cleansed')
