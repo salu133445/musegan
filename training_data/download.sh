a80c1bbddd5f057e1571a011e146718526a82b78
Hao-Wen Dong <salu133445@citi.sinica.edu.tw>
2018-05-11 00:24:57 +0800

    Fix google drive large file confirmation issue

--- training_data/download.sh
+++ training_data/download.sh
@@ -21,5 +21,10 @@ case $filename in
     ;;
 esac
 
-wget -O $filename --no-check-certificate \
-  "https://drive.google.com/uc?export=download&id="$fileid
+confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
+  --no-check-certificate \
+  "https://docs.google.com/uc?export=download&id=$fileid" -O- | sed -rn \
+  's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget -O $filename --load-cookies /tmp/cookies.txt \
+  "https://docs.google.com/uc?export=download&confirm=$confirm&id=$fileid"
+rm -rf /tmp/cookies.txt
