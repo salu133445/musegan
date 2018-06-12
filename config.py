5f886ba88d666b72f06f0ee70dbd2e995899fc56
Hao-Wen Dong <salu133445@citi.sinica.edu.tw>
2018-06-12 17:06:05 +0800

    Change default number of epoch to 10

--- config.py
+++ config.py
@@ -193,7 +193,7 @@ for key in ('training_data', 'training_data_location'):
 #=========================== Training Configuration ============================
 #===============================================================================
 CONFIG['train'] = {
-    'num_epoch': 20,
+    'num_epoch': 10,
     'verbose': None,
     'sample_along_training': None,
     'evaluate_along_training': None,
