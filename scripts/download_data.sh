bbbd9f9a7cde3389e4b3194a2543eebeb47870a4
Hao-Wen Dong <salu.hwdong@gmail.com>
2021-01-04 23:22:31 +0800

    Use gdown to download files from gdrive

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 # This script download the training data.
 # Usage: download_data.sh
-download () {
-  confirm=$(wget --quiet --save-cookies /tmp/cookies.txt \
-    --keep-session-cookies --no-check-certificate \
-    "https://docs.google.com/uc?export=download&id=$2" -O- | sed -rn \
-    's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-  wget -O "$1" --load-cookies /tmp/cookies.txt \
-    "https://docs.google.com/uc?export=download&confirm=$confirm&id=$2"
-  rm -rf /tmp/cookies.txt
-}
+#
+# Make sure `gdown` is installed, otherwise install it by `pip install gdown`.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 if [ ! -d "$DIR/../data" ]; then
   mkdir "$DIR/../data"
 fi
-download "$DIR/../data/train_x_lpd_5_phr.npz" \
-  "14rrC5bSQkB9VYWrvt2IhsCjOKYrguk3S"
+gdown -O "$DIR/../data/train_x_lpd_5_phr.npz" \
+  --id "14rrC5bSQkB9VYWrvt2IhsCjOKYrguk3S"
--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 # This script download the pretrained models.
 # Usage: download_models.sh
-download () {
-  confirm=$(wget --quiet --save-cookies /tmp/cookies.txt \
-    --keep-session-cookies --no-check-certificate \
-    "https://docs.google.com/uc?export=download&id=$2" -O- | sed -rn \
-    's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-  wget -O "$1" --load-cookies /tmp/cookies.txt \
-    "https://docs.google.com/uc?export=download&confirm=$confirm&id=$2"
-  rm -rf /tmp/cookies.txt
-}
+#
+# Make sure `gdown` is installed, otherwise install it by `pip install gdown`.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 if [ ! -d "$DIR/../exp" ]; then
   mkdir "$DIR/../exp"
 fi
-download "$DIR/../exp/pretrained_models.tar.gz" \
-  "19RYAbj_utCDMpU7PurkjsH4e_Vy8H-Uy"
+gdown -O "$DIR/../exp/pretrained_models.tar.gz" \
+  --id "19RYAbj_utCDMpU7PurkjsH4e_Vy8H-Uy"
 echo "Decompressing archive."
 if tar zxf "$DIR/../exp/pretrained_models.tar.gz" -C "$DIR/../exp"; then
     rm "$DIR/../exp/pretrained_models.tar.gz"
